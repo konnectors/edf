1914a54ce5dbf3cbd6fcffc6e7b3b5b00777022a
doubleface <christophe@cozycloud.cc>
2023-11-21 18:53:45 +0100

    fix: wait for page reload done before waiting for selector

--- src/index.js
+++ src/index.js
@@ -1022,6 +1022,7 @@ class EdfContentScript extends ContentScript {
               `Retrying ${label}, attempt ${error.attemptNumber} on Failed to fetch error`
             )
             await this.evaluateInWorker(() => window.location.reload())
+            await new Promise(resolve => window.setTimeout(resolve, 1000)) // wait for reload command to be given to worker
             await this.waitForElementInWorker(selectorToWait)
           } else {
             throw error
